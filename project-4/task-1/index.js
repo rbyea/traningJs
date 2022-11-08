136f21603cd2cc7d978849ad50912deb7d6f275e
Rbye <42256950+rbyea@users.noreply.github.com>
2022-11-08 16:26:15 +0300

    project-4 task-1

---
+++ project-4/task-1/index.js
@@ -0,0 +1,16 @@
+let num = 9;
+let text = 'Text';
+let checked = true;
+let status;
+let obj = {};
+let inputValue = null;
+let id = Symbol('id');
+let bigNumber = 1000n
+
+console.log(Number(num), String(num), Boolean(num))
+console.log(Number(text), String(text), Boolean(text))
+console.log(Number(checked), String(checked), Boolean(checked))
+console.log(Number(status), String(status), Boolean(status))
+console.log(Number(obj), String(obj), Boolean(obj))
+console.log(Number(inputValue), String(inputValue), Boolean(inputValue))
+console.log(Number(bigNumber), String(bigNumber), Boolean(bigNumber))
