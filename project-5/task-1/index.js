6eb10d74cd46536c5af3cea26ac149c91d7643ed
Rbye <42256950+rbyea@users.noreply.github.com>
2022-11-09 00:42:43 +0300

    project-5 task-1

---
+++ project-5/task-1/index.js
@@ -0,0 +1,9 @@
+let myName = 'Егор';
+let programmingLanguage = 'JavaScript';
+let courseCreatorName = 'Владилена Минина';
+let reasonText = 'верстальщик ;D';
+let numberOfMonth = 'на практике приходилось костылить)';
+
+let result = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я не изучал ${programmingLanguage} ${numberOfMonth}. Я уверен, что пройду данный курс до конца!`;
+
+console.log(result)
