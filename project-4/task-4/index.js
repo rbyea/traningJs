bf2bb180463aa157a6917aad761df03077d047e6
Rbye <42256950+rbyea@users.noreply.github.com>
2022-11-08 17:58:37 +0300

    project-4 task 4

---
+++ project-4/task-4/index.js
@@ -0,0 +1,20 @@
+// 1
+console.log(Number(' 1 2 3 4 5'));
+// 2
+console.log(Number('1234 5'));
+// 3
+console.log(Number('12345'));
+// 4
+console.log(String(false));
+// 5
+console.log(Boolean(0000000));
+// 6
+console.log(Boolean(0.0000001));
+// 7
+console.log(String(undefined));
+// 8
+console.log(Number('100f'));
+// 9
+console.log(Number('100'));
+// 10
+console.log(Number('000001'));
