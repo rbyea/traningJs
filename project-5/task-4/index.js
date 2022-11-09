5e6261ff6f33afebeadfe6c6cb50d0c027c00bad
Rbye <42256950+rbyea@users.noreply.github.com>
2022-11-09 22:54:23 +0300

    p5 task-4

---
+++ project-5/task-4/index.js
@@ -0,0 +1,11 @@
+let userName = prompt('Как ваз зовут?');
+let userAge = prompt('Сколько вам лет?');
+let userNameUpper = userName.toUpperCase();
+let userNameTrim = userNameUpper.trim();
+let userAgeTrim = userAge.trim();
+let userAgeNumber = Number(userAgeTrim);
+
+let resultUser = userNameTrim;
+let resultAge = userAgeNumber;
+
+alert(`Вас ховут ${resultUser} и вам ${resultAge}`);
