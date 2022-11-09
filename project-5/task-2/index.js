cd73d4c52699924cfb58d1f8aad32927261f01de
Rbye <42256950+rbyea@users.noreply.github.com>
2022-11-09 12:32:11 +0300

    result

--- project-5/task-2/index.js
+++ project-5/task-2/index.js
@@ -7,6 +7,6 @@ let numberOfMonth = 'на практике приходилось костыли
 let myInfoText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я не изучал ${programmingLanguage} ${numberOfMonth}. Я уверен, что пройду данный курс до конца!`;
 
 let replaceTextJs = myInfoText.replaceAll('JavaScript', 'javascript');
-// myInfoText.replaceAll('курс', 'КУРС');
+// myInfoText.replaceAll('курс', 'КУРсс');
 
 console.log(myInfoText.indexOf('JavaScript'));
