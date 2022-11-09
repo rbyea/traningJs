83770ca031316660f5b50a921fbb4282e97e609f
Rbye <42256950+rbyea@users.noreply.github.com>
2022-11-09 12:22:37 +0300

    test

---
+++ project-5/task-2/index.js
@@ -0,0 +1,12 @@
+let myName = 'Егор';
+let programmingLanguage = 'JavaScript';
+let courseCreatorName = 'Владилена Минина';
+let reasonText = 'верстальщик ;D';
+let numberOfMonth = 'на практике приходилось костылить)';
+
+let myInfoText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я не изучал ${programmingLanguage} ${numberOfMonth}. Я уверен, что пройду данный курс до конца!`;
+
+let replaceTextJs = myInfoText.replaceAll('JavaScript', 'javascript');
+// myInfoText.replaceAll('курс', 'КУРС');
+
+console.log(myInfoText.indexOf('JavaScript'));
