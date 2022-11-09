8471d75f138f6d2d31b6f7fcfe822da993ffaa67
Rbye <42256950+rbyea@users.noreply.github.com>
2022-11-09 22:30:27 +0300

    p5 task-3

---
+++ project-5/task-3/index.js
@@ -0,0 +1,9 @@
+let userName = prompt('Как ваз зовут?');
+let userNameUpper = userName.toUpperCase();
+let userNameTrim = userNameUpper.trim();
+let result = userNameTrim;
+
+alert(`Вас ховут ${result}`)
+
+
+// Не знаю на сколько это правильно. Но по моему мнению конечный результат добавлять в переменную result нужно) В прошлой таске я так не делал.
