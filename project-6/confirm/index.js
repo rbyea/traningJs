4ae99ee80827e338c86863a835776e0e92d9cef8
Rbye <42256950+rbyea@users.noreply.github.com>
2022-11-10 20:49:03 +0300

    pr6 task-3

---
+++ project-6/confirm/index.js
@@ -0,0 +1,3 @@
+confirm('JavaScript появился в 1995 году') ? alert('Верно') : alert('JavaScript появился в 1995 году');
+confirm('Спецификация JavaScript называется ECMAScript?') ? alert('Верно') : alert('Спецификация JavaScript называется ECMAScript');
+confirm('JavaScript был создан за 1 месяц?') ? alert('JavaScript был создан за 10 дней') : alert('Верно');
