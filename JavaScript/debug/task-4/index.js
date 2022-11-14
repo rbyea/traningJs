be575f9a13d78e7dfa89312226432a84c5325495
Rbye <42256950+rbyea@users.noreply.github.com>
2022-11-15 00:39:36 +0300

    debuger task-4

---
+++ JavaScript/debug/task-4/index.js
@@ -0,0 +1,17 @@
+let numbers = [10, 4, 100, -5, 54, 2]
+let sum = 0;
+
+// Через цикл for
+for (let i = 0; i < numbers.length; i += 1) {
+    numbers[i] = numbers[i] ** 3;
+    sum += numbers[i];
+}
+console.log(sum); // 1158411
+
+// Через цикл for of
+sum = 0;
+for (let num of numbers){
+    // num = num ** 3;
+    sum += num;
+}
+console.log(sum); // 1003904306910622800
