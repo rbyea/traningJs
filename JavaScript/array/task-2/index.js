81dc173dba95aaa35ff33b7692e0df634d57130a
Rbye <42256950+rbyea@users.noreply.github.com>
2022-11-16 16:46:41 +0300

    array task-2

---
+++ JavaScript/array/task-2/index.js
@@ -0,0 +1,16 @@
+const getSumOfSequence = number => {
+    let num = [];
+    let sum = 0;
+
+    for(let i = 1; i<=number; i++) {
+        num.push(i)
+    }
+
+    sum = num[0] + num[num.length-1]
+
+    return sum
+}
+
+let result = getSumOfSequence(5)
+
+console.log(result)
