6ad108571954f34ba7678b65e2bcc87de6369b44
Rbye <42256950+rbyea@users.noreply.github.com>
2022-11-25 19:20:49 +0300

    obj task-6

---
+++ JavaScript/object/task-6/index.js
@@ -0,0 +1,40 @@
+const groceries = {
+    "73Wakv": {
+        name: "Orange Juice",
+        price: 1.5,
+        discount: 10
+    },
+    "5L3db9": {
+        name: "Chocolate",
+        price: 2,
+        discount: 0
+    }
+};
+const shoppingBag = [
+    { productId: "73Wakv", count: 3 },
+    { productId: "5L3db9", count: 23 }
+];
+
+const getTotalPriceOfShoppingBag = (shoppingBagArray) => {
+    let allPrice = 0;
+    let priceDiscount = 0;
+
+    for (const shoppingBagArrayElement of shoppingBagArray) {
+        let shoppingId = shoppingBagArrayElement.productId;
+        let shoppingCount = shoppingBagArrayElement.count;
+        let groceriesPrice = groceries[shoppingId].price;
+        let groceriesCount = groceries[shoppingId].discount;
+
+        allPrice += groceriesPrice * shoppingCount
+        if(groceriesCount > 0) {
+            priceDiscount += groceriesPrice * shoppingCount * groceriesCount / 100
+        }
+    }
+
+    let result = allPrice - priceDiscount
+
+    return result.toFixed(2)
+}
+
+const totalPrice = getTotalPriceOfShoppingBag(shoppingBag);
+console.log("totalPrice", totalPrice); // Возвращает 50.05
