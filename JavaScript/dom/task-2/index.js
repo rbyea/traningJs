f99ae7ef4f7a53e31f74852ce503730b56221fd0
Rbye <42256950+rbyea@users.noreply.github.com>
2022-12-07 13:04:35 +0300

    dom task-2

--- JavaScript/dom/task-2/index.js
+++ JavaScript/dom/task-2/index.js
@@ -17,36 +17,25 @@ const tasks = [
 ];
 const tasksList = document.querySelector('.tasks-list')
 
-tasks.forEach((el) => {
-    const taskItem = document.createElement('div');
-    taskItem.classList.add('task-item')
-    taskItem.setAttribute('data-task-id', `${el.id}`)
-    const taskItemContainer = document.createElement('div')
-    taskItemContainer.classList.add('task-item__main-container')
-    const taskItemContent = document.createElement('div')
-    taskItemContent.classList.add('task-item__main-content')
-    const form = document.createElement('form')
-    form.classList.add('checkbox-form')
-    const input = document.createElement('input')
-    input.classList.add('checkbox-form__checkbox')
-    input.setAttribute('type', `checkbox`)
-    input.setAttribute('id', `${el.id}`)
-    input.checked = el.completed
-    const label = document.createElement('label')
-    label.setAttribute('for', `${el.id}`)
-    const  span = document.createElement('span')
-    span.classList.add('task-item__text')
-    span.innerHTML = `${el.text}`
-    const button = document.createElement('button')
-    button.classList.add('task-item__delete-button', 'default-button', 'delete-button')
-    button.innerHTML = 'Удалить'
-
-    tasksList.append(taskItem)
-    taskItem.append(taskItemContainer)
-    taskItemContainer.append(taskItemContent)
-    taskItemContainer.append(button)
-    taskItemContent.append(form)
-    form.append(input)
-    form.append(label)
-    taskItemContent.append(span)
+const task = tasks.map((task) => {
+    return (
+        `<div class="task-item" data-task-id="${task.id}">
+        <div class="task-item__main-container">
+            <div class="task-item__main-content">
+                <form class="checkbox-form">
+                    <input class="checkbox-form__checkbox" type="checkbox" ${task.completed ? checked : ''} id="${task.id}">
+                    <label for="${task.id}"></label>
+                </form>
+                <span class="task-item__text">
+                    ${task.text}
+                </span>
+            </div>
+            <button class="task-item__delete-button default-button delete-button" data-delete-task-id="${task.id}">
+             Удалить
+            </button>
+        </div>
+    </div>`
+    )
 })
+
+tasksList.innerHTML = task.join('')
