5e3e37bea1559fe86f16cadd8de9bffa455f3bd5
rbye rbye <fl.rbyave@gmail.com>
2022-12-08 18:19:56 +0300

    dom task-3

--- JavaScript/dom/task-3/index.js
+++ JavaScript/dom/task-3/index.js
@@ -18,27 +18,32 @@ const tasks = [
 
 const createTaskForm = document.querySelector('.create-task-block')
 const tasksList = document.querySelector('.tasks-list')
+const spanError = document.querySelector('.error-message-block')
 createTaskForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const {target} = e
     const taskNameInput = target.taskName
-    taskNameInput.value
-        ? tasks.push({
-            id: String(Date.now()),
-            completed: false,
-            text: taskNameInput.value,
+    let pushEl = false
+
+    if(taskNameInput.value.length > 0) {
+        tasks.forEach(el => {
+            if(taskNameInput.value === el.text) {
+                pushEl = true
+            }
         })
-        : console.log('Введите название задачи')
 
-    taskNameInput.value = ''
-    taskNameInput.focus()
-
-
-    console.log(true)
-
-    const task = tasks.map((task) => {
-        return (
-            `<div class="task-item" data-task-id="${task.id}">
+        if(pushEl) {
+            spanError.innerHTML = 'Задача с таким названием уже существует.'
+            spanError.classList.add('active')
+        } else {
+            tasks.push({
+                id: String(Date.now()),
+                completed: false,
+                text: taskNameInput.value,
+            })
+            const task = tasks.map((task) => {
+                return (
+                    `<div class="task-item" data-task-id="${task.id}">
                 <div class="task-item__main-container">
                     <div class="task-item__main-content">
                         <form class="checkbox-form">
@@ -50,13 +55,21 @@ createTaskForm.addEventListener('submit', (e) => {
                         </span>
                     </div>
                     <button class="task-item__delete-button default-button delete-button" data-delete-task-id="${task.id}">
-                     Удалить
+                        Удалить
                     </button>
                 </div>
             </div>`
-        )
-    })
+                )
+            })
+            tasksList.innerHTML = task.join('')
+            spanError.classList.remove('active')
+        }
+    } else {
+        spanError.innerHTML = 'Название задачи не должно быть пустым'
+        spanError.classList.add('active')
+    }
 
-    tasksList.innerHTML = task.join('')
+    taskNameInput.value = ''
+    taskNameInput.focus()
 })
 
