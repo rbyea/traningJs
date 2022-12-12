d7af31b278f20eb2ff4c9543e18a941fbb88bf2e
rbye rbye <fl.rbyave@gmail.com>
2022-12-12 11:43:36 +0300

    dom task-6 starta

---
+++ JavaScript/dom/task-6/index.js
@@ -0,0 +1,11 @@
+const options = [
+    { value: 1, text: 'JavaScript' },
+    { value: 2, text: 'NodeJS' },
+    { value: 3, text: 'ReactJS' },
+    { value: 4, text: 'HTML' },
+    { value: 5, text: 'CSS' }
+];
+
+const customSelect = new CustomSelect('123', options);
+const mainContainer = document.querySelector('#container');
+customSelect.render(mainContainer)
