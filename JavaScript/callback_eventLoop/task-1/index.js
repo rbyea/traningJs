083cc66d6d813cad35df18dc7037d33f6efe37f7
Rbye <42256950+rbyea@users.noreply.github.com>
2022-12-23 14:35:45 +0300

    evenloop tasks 1,2,3

---
+++ JavaScript/callback_eventLoop/task-1/index.js
@@ -0,0 +1,13 @@
+setTimeout(() => {
+    console.log('setTimeout');
+}, 0);
+const promise = new Promise((resolve) => {
+    console.log('Promise');
+    resolve();
+});
+promise.then(() => {
+    console.log('Promise resolve');
+});
+console.log('End');
+
+// promise/ end / promise resolve \ setTimeout()
