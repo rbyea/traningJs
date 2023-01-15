2c4481a732a272296e5a1a669e9e256e47230cc2
Rbye <42256950+rbyea@users.noreply.github.com>
2023-01-15 14:33:55 +0300

    js task-4

--- JavaScript/js/task-4/index.js
+++ JavaScript/js/task-4/index.js
@@ -1,1 +1,20 @@
-console.log(true)
+let newStr = ''
+
+const isPalindrome = (str) => {
+    console.log(str)
+    for(let i = str.length-1; i>=0; i--) {
+        newStr += str[i]
+    }
+
+    newStr === str ? console.log(true) : console.log(false)
+    newStr = '';
+}
+
+isPalindrome('racecar') // true
+isPalindrome('programmer') // false
+isPalindrome('топот') // true
+isPalindrome('дед') // true
+isPalindrome('доход') // true
+isPalindrome('калаш') // false
+
+
