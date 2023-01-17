7254b267f9f70c734342a1dfdf1469d5da0f04e1
Novikov Egor <42256950+rbyea@users.noreply.github.com>
2023-01-18 00:16:44 +0300

    js task-5

--- JavaScript/js/task-5/src/app.js
+++ JavaScript/js/task-5/src/app.js
@@ -1,12 +1,7 @@
 import {Donations} from "./utils/Donations.js";
 
 const initApp = () => {
-    const input = document.querySelector('.donate-form__donate-input');
-    const button = document.querySelector('.donate-form__submit-button');
-    const form = document.querySelector('.donate-form');
-    const container = document.querySelector('.donates-container__donates');
-
-    const obj = new Donations(input, button, form, container)
+    const obj = new Donations()
     obj.btnEvent()
 }
 
--- JavaScript/js/task-5/src/utils/Donations.js
+++ JavaScript/js/task-5/src/utils/Donations.js
@@ -1,20 +1,43 @@
 export class Donations {
-    constructor(inputName, buttonName,form, container) {
-        this.valueInput = inputName;
-        this.button = buttonName;
-        this.form = form;
-        this.container = container;
-    }
-    btnEvent() {
-        const totalPrice = document.querySelector('#total-amount')
+    #input = document.querySelector('.donate-form__donate-input');
+    #button = document.querySelector('.donate-form__submit-button');
+    #form = document.querySelector('.donate-form');
+    #container = document.querySelector('.donates-container__donates');
+    #totalPrice = document.querySelector('#total-amount');
+    #donatError = document.querySelector('.donate-error');
+    #balance = 0;
+    #monthNames = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
 
-        this.form.addEventListener('click', (e) => {
+    btnEvent() {
+        this.#form.addEventListener('click', (e) => {
             e.preventDefault();
             const {target} = e
+            const inputValue = +this.#input.value
 
-            if(target.className.includes(this.button.className)) {
-                console.log(this.valueInput.value)
+            if(target.className.includes(this.#button.className)) {
+                if(inputValue >= 1 && inputValue <= 100) {
+                    this.#balance += inputValue
+                    this.#donatError.classList.remove('active')
+                    this.#input.value = ''
+                    this.renderList(inputValue, new Date())
+                } else {
+                    this.#donatError.classList.add('active')
+                }
             }
+            this.#totalPrice.innerHTML = `${this.#balance}$`
         })
+    }
+
+    renderList(donat, time) {
+        const getMonth = time.getMonth()
+        const getDate = time.getDate()
+        const getYear = time.getFullYear()
+        const getTime = time.toLocaleTimeString()
+
+        const div = document.createElement('div')
+        div.className = 'donate-item'
+        div.innerHTML = `${this.#monthNames[getMonth]}, ${getDate}th, ${getYear}, ${getTime} - <b>${donat}$</b>`
+
+        this.#container.append(div)
     }
 }
