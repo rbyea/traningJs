5319b4036c68d1a4e1219ecb63752523c242c35e
Novikov Egor <csgo.roby@gmail.com>
2023-03-07 17:41:56 +0300

    refactor

--- fastcompany/my-app/src/components/Status/Status.jsx
+++ fastcompany/my-app/src/components/Status/Status.jsx
@@ -5,6 +5,29 @@ import PropTypes from "prop-types";
 const Status = ({ users }) => {
   const dancePeople = `${users.length} человек тусанет с тобой сегодня`;
 
+  const convertEnding = (number) => {
+    if (number === 0) {
+      return "Никто с тобой не тусанет";
+    }
+  
+    const lastDigit = number % 10;
+    const lastTwoDigits = number % 100;
+  
+    if (lastTwoDigits > 10 && lastTwoDigits < 20) {
+      return `${number} человек тусанет с тобой сегодня`;
+    }
+  
+    if (lastDigit === 1) {
+      return `${number} человек тусанет с тобой сегодня`;
+    }
+  
+    if (lastDigit >= 2 && lastDigit <= 4) {
+      return `${number} человека тусанет с тобой сегодня`;
+    }
+  
+    return `${number} человек тусанет с тобой сегодня`;
+  };
+
   return (
     <Box
       sx={{
@@ -20,13 +43,9 @@ const Status = ({ users }) => {
       }}
       className={users.length === 0 ? "box-active" : ""}
     >
-      {users.length > 4
-        ? dancePeople
-        : users.length === 1
-          ? dancePeople
-          : users.length === 0
-            ? `Никто с тобой не тусанет`
-            : `${users.length} человека тусанет с тобой сегодня`}
+      {
+        convertEnding(users.length)
+      }
     </Box>
   );
 };
